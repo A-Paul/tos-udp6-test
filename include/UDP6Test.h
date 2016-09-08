27d16506d92768ea0b5a2739e88812a849d954b3
Andreas "Paul" Pauli <andreas.pauli@haw-hamburg.de>
2016-09-08 18:47:02 +0200

    UDPTest.h: More choices for target

--- include/UDP6Test.h
+++ include/UDP6Test.h
@@ -2,7 +2,13 @@
 #define UDP6TEST_H
 
 #define UDP6_TEST_PORT 23666
-/* Target node */
-#define UDP6_TEST_TGT_IN6 "fe80::212:6d45:509c:42a9"
+#define UDP6_TEST_TGT_IN6 Z1_SPECIFIC_IN6
+/* Target node(s) */
+/* telosb #14 */
+#define TELOS_SPECIFIC_IN6 "fe80::212:6d45:509c:42a9"
+/* Some iotlab-m3 under gnrc_networking@RIOT */
+#define IOTM3_SPECIFIC_IN6 "fe80::3432:4833:46d8:9e2a"
+/* Arbitrary Z1 built with ``install.2`` */
+#define Z1_SPECIFIC_IN6 "fe80::212:6d4c:4f00:2"
 
 #endif /* UDP6TEST_H */
